bd3e04a5b9a9306bed2da3564fc6d80f67d5e77c
JesseColon <jay.alexandervii@gmail.com>
2023-08-20 14:01:40 -0400

    attempting to fix client side js files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,8 +2,10 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+    
     window.deferredPrompt = event;
+
+    butInstall.classList.toggle('hidden', false);
 });
 
 butInstall.addEventListener('click', async () => {
